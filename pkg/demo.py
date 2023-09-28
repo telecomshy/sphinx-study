be1be60e173a093c80d8059f0923557c7ea4e065
admin <18907190693@189.cn>
2023-09-28 17:49:04 +0800

    add pkg demo

---
+++ pkg/demo.py
@@ -0,0 +1,25 @@
+def func(x: float, y: float) -> float:
+    """
+    it's demo of function's docstring
+
+    Parameters
+    ----------
+    x
+        number
+    y
+        a number two
+
+    Returns
+    -------
+    the sum of a and b
+
+    Examples
+    --------
+
+    >>> func(1, 3)
+    4
+
+    """
+
+    return x + y
+
